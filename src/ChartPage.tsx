f3c275578f98072ce11824c4e9698508f75179fa
Masui <masanori840816@gmail.com>
2024-10-07 02:43:29 +0900

    update radar design

--- src/ChartPage.tsx
+++ src/ChartPage.tsx
@@ -2,7 +2,7 @@ import "./ChartPage.css"
 import { RaderChart } from "./RaderChart"
 
 export function ChartPage(): JSX.Element {
-    return <div className="chart-area">
+    return <div className="bg-gray w-screen h-screen">
         <RaderChart />
     </div>
 }
--- src/RaderChart.tsx
+++ src/RaderChart.tsx
@@ -10,40 +10,68 @@ import {
   } from 'chart.js';
   ChartJS.register(RadialLinearScale, PointElement, LineElement, Filler, Tooltip, Legend);
 
-export function RaderChart(): JSX.Element {
+  export function RaderChart(): JSX.Element {
     const data = {
-        labels: ['Running', 'Swimming', 'Eating', 'Cycling', 'Sleeping'],
+        labels: [
+          ["Running", "90"], 
+          ["Swimming", "10"],
+          ["Eating", "4"],
+          ["Cycling", "52"],
+          ["Sleeping", "15"],
+        ],
         datasets: [
           {
-            label: 'John',
             data: [90, 10, 4, 52, 15],
-            backgroundColor: 'rgba(255, 99, 132, 1)',
-            borderColor: 'rgba(255, 99, 132, 1)',
+            backgroundColor: 'rgba(255, 255, 132, 0.6)',
+            borderColor: 'rgba(255, 99, 244, 1)',
             borderWidth: 1,
+            pointRadius: 0,
           },
         ],
       };
     
       const options: any = {
+        type: 'radar',
         scale: {
-          ticks: { beginAtZero: true,
+          ticks: {
+            beginAtZero: true,
             min: 0,
-        max: 100,
-        stepSize: 20,
+            max: 100,
+            stepSize: 20,
            },
+        },
+        scales: {
+          r: {
+            grid: {
+              circular: false,
+              lineWidth: 5,
+              color: "rgba(255, 250, 25, 0.9)",
+            },
+            angleLines: {
+              color: 'rgba(0, 0, 0, 0.5)',
+              lineWidth: 2,
+            },
+            pointLabels: {
+              font: {
+                size: 16,
+              },
+              padding: 10,
+              display: true,
+            },
+            ticks: {
+              display: false,
+            },
+          },
         },
         plugins: {
           legend: {
-            
             display: false,
-            position: 'top',
           },
           title: {
             display: false,
-            text: 'Radar Chart Example',
           },
         },
       };
     
-      return <Radar data={data} options={options} />;
+      return <Radar data={data} options={options} className="bg-white w-full h-full" />;
 }
